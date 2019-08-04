483a547a4721f4c02282c8ed17ad7a86afdf91c6
HSR <hectorsilvarobles@gmail.com>
2019-08-04 15:05:36 -0700

    day 1 project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+
       </header>
+        <div className='banner'>
+          <div className='left-side'>
+            <h1>Whats Good?</h1>
+            <h2>001</h2>
+          </div>
+          <div className='right-side'>
+            <div className='orange-box'></div>
+            <img src='https://www.stickpng.com/assets/thumbs/58418bf8a6515b1e0ad75a42.png' />
+            <div className='text-on-left'>
+              <h2>UI 30 Days</h2>
+              <p>I will be challenging myself to design and create User Interfaces everyday for next 30 days. Wish me luck!</p>
+            </div>
+            <div className='text-on-right'>
+              <h2>King Of The Jungle</h2>
+              <div></div>
+            </div> 
+          </div>
+        </div>
+        <div className='more'>Hello</div>
     </div>
   );
 }
